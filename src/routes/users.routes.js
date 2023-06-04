e36669992565f34d07d101101fb0108e967b569b
Guillou <henri.guillou97@gmail.com>
2023-06-04 17:58:01 +0200

    ok

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,8 +5,8 @@ import { jwtMiddleware } from "../middlewares/jwt.middleware.js";
 const initUsersRoutes = (app, sm) => {
   const router = Router();
   router.get("/readAll", sm,UserController.read);
-  router.get("/readById/:id", sm,  UserController.getUserInfos);
-  router.put("/update/:id", sm, UserController.update)
+  router.get("/readById/:id",jwtMiddleware, sm,  UserController.getUserInfos);
+  router.put("/update/:id",jwtMiddleware, sm, UserController.update)
   router.post("/sign-up", sm, UserController.signUp);
   router.post("/sign-in", sm, UserController.signIn);
   router.delete("/delete/:id",jwtMiddleware, sm, UserController.dltUser);
--- src/routes/vehicles.routes.js
+++ src/routes/vehicles.routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { VehicleController } from "../controllers/vehicle.controller.js";
-//import { jwtMiddleware } from "../middlewares/jwt.middleware.js";
+import { jwtMiddleware } from "../middlewares/jwt.middleware.js";
 
 const initVehiclesRoutes = (app, sm) => {
   const router = Router();
-  router.post("/add",sm, VehicleController.createVehicle);
+  router.post("/add",jwtMiddleware,sm, VehicleController.createVehicle);
   router.get("/all",sm, VehicleController.readVehicles);
-  router.put("/:id/active", sm, VehicleController.updateActive);
+  router.put("/:id/active",jwtMiddleware, sm, VehicleController.updateActive);
   router.get("/:id",sm, VehicleController.readVehicleById);
   router.get("/type/:type_id",sm, VehicleController.readVehicleByTypeId);
   app.use("/vehicles", router);
 };
+
 
 export default initVehiclesRoutes;
